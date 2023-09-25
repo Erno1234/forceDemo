8c36137ea182443b5910ee4c914afe8c20394635
Erno1234 <115622555+Erno1234@users.noreply.github.com>
2023-09-25 15:21:48 +0200

    Update JavaApplication3.java

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -5,12 +5,12 @@ public class JavaApplication3 {
     public static void main(String[] args) {
 
 <<<<<<< HEAD
-        for (int i = 0; i < 10; i++) {
+        while (int i = 0; i < 10; i++) {
              if (true) {
 
             }
 =======
-        while (true) {            
+        for (true) {            
 
         if (true) {
             
